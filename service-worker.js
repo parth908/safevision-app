e144a17d729a8f603b08d475ec836d8a1ed384e2
parth908 <parthvats2342@gmail.com>
2025-06-15 16:54:00 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,10 @@
 const CACHE_NAME = 'safevision-cache-v1';
 const urlsToCache = [
-  './', // Caches the root, i.e., index.html
+  './',
   './index.html',
   './manifest.json',
   './icon-192.png',
   './icon-512.png',
-  // It's generally better to let CDN resources be fetched from network
-  // and not cache them aggressively here, unless you have specific offline requirements
-  // that justify the complexity of caching cross-origin resources.
-  // 'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs',
-  // 'https://cdn.jsdelivr.net/npm/@tensorflow-models/coco-ssd'
 ];
 
 self.addEventListener('install', (event) => {
@@ -26,27 +21,19 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
       .then((response) => {
-        // Cache hit - return response
         if (response) {
           return response;
         }
         return fetch(event.request).then(
           function(response) {
-            // Check if we received a valid response
             if(!response || response.status !== 200 || response.type !== 'basic') {
               return response;
             }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and can only be consumed once. We must clone it so that
-            // the browser can consume the original response and we can consume the clone.
             var responseToCache = response.clone();
-
             caches.open(CACHE_NAME)
               .then(function(cache) {
                 cache.put(event.request, responseToCache);
               });
-
             return response;
           }
         );
